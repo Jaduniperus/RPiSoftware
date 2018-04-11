278d23e40b9127ff0fc6d78cb731d39f1284ad22
SpellAudio <philippe.guelen@gmail.com>
2018-04-11 16:59:01 +0200

    Fonctionnement 95% du TDA, pb avec diminuer le volume de la bande

--- src/main.cpp
+++ src/main.cpp
@@ -48,36 +48,10 @@ using namespace std;
 *
 * PULL UP sur SDA et SCL
 */
-
 int main() 
 {
-
   // Définition des variables du programme
   int state = 0; // Niveau du relais: 1 = fermé (contact) 0 = ouvert (pas contact)
-  int commandTDA[21];
-
-  commandTDA[0] = 0x00; // 0.375db step // 0
-  commandTDA[1] = 0x01; // -0.372
-  commandTDA[2] = 0x02; // -0.75
-  commandTDA[3] = 0x03; // -1.125
-  commandTDA[4] = 0x04; // -1.5
-  commandTDA[5] = 0x05; // -1.875
-  commandTDA[6] = 0x06; // -2.25
-  commandTDA[7] = 0x07; // -2.625
-  commandTDA[8] = 0x00; // 3 dB step // 0
-  commandTDA[9] = 0x08; //-3
-  commandTDA[10] = 0x09; // -6
-  commandTDA[11] = 0x10; // -9
-  commandTDA[12] = 0x11; // -12
-  commandTDA[13] = 0x12; // -15
-  commandTDA[14] = 0x80; // 2 dB step // 0
-  commandTDA[15] = 0x81; // 2 dB
-  commandTDA[16] = 0x82; // 4 dB
-  commandTDA[17] = 0x83; // 6 dB
-  commandTDA[18] = 0x81; // 8 dB
-  commandTDA[19] = 0x82; // 10 dB
-  commandTDA[20] = 0x83; // 12 dB
-  commandTDA[21] = 0x81; // 14 dB
 
   // Initialisation de wiringPi
     if(wiringPiSetup() == -1) //when initialize wiringPi failed, print message to screen
@@ -85,7 +59,6 @@ int main()
     printf("setup wiringPi failed !\n");
     return -1; 
   }
-
 
   // Initialisation des périphériques i2c au moyen de wiringPiI2CSetup
   int devIdTDABass = 0x43;
@@ -101,7 +74,7 @@ int main()
   // test validant la création de l'objet I2C
     if(I2C_TDATREBL < 0) cout << "Error. TDA trebble Errno is: " << errno << endl;
     else  cout << "Init result for TDA trebble: "<< I2C_TDATREBL << endl;
-
+  */
 /*
   // Initialisation des périphériques SPI au moyen de wiringPiSPISetup
   int devIdAD1;
@@ -124,13 +97,13 @@ int main()
   pullUpDnControl(8, PUD_UP); //permet de configurer une pin en mode pull-up, pull-down ...
   pullUpDnControl(9, PUD_UP);
   // Boucle infinie du programme
+  int value;
   do
   {
     cin >> state;
-    wiringPiI2CWrite(I2C_TDABASS,state);
+    value = wiringPiI2CWrite(I2C_TDABASS,state);
+    cout << value << endl;
   }
   while(1);
-  
     return 0;
 }
-
