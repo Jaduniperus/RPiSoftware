dbd31245a589ac7592451bb0e2a5da281ffc12ec
SpellAudio <philippe.guelen@gmail.com>
2018-04-11 11:45:05 +0200

    Ecriture du TDA I2C part 1

--- src/main.cpp
+++ src/main.cpp
@@ -33,11 +33,60 @@ using namespace std;
 // Definition des pin GPIO utilisées
 #define RelayPin 29
 
+/* I2C Start condition
+* SCL HIGH 
+* SDA HIGH -> LOW
+*
+* I2C Stop condition
+* SCL LOW -> HIGH
+* SDA LOW -> HIGH
+*
+* Ne jamais modifier l'état de SCL pendant un changement d'état de SDA
+* Modifier SDA que quand SCL est LOW
+* 
+* Mettre SDA en HIGH au moment du ACKNOWLEDGE. LE TDA passera SDA à LOW si ACK
+*
+* PULL UP sur SDA et SCL
+*/
+
+int TDAVolume(char command)
+{
+  if(command == '0,375')
+}
+
 int main() 
 {
 
   // Définition des variables du programme
-  int state = 0;
+  int state = 0; // Niveau du relais: 1 = fermé (contact) 0 = ouvert (pas contact)
+  int commandTDA[18];
+
+  commandTDA[0] = 0x00;
+  commandTDA[1] = 0x01;
+  commandTDA[2] = 0x02;
+  commandTDA[3] = 0x03;
+  commandTDA[4] = 0x04;
+  commandTDA[5] = 0x05;
+  commandTDA[6] = 0x06;
+  commandTDA[7] = 0x07;
+  commandTDA[8] = 0x00;
+  commandTDA[9] = 0x08;
+  commandTDA[10] = 0x09;
+  commandTDA[11] = 0x10;
+  commandTDA[12] = 0x11;
+  commandTDA[13] = 0x12;
+  commandTDA[14] = 0x13;
+  commandTDA[15] = ;
+  commandTDA[16] = ;
+  commandTDA[17] = ;
+
+
+
+
+
+
+  }
+
   // Initialisation de wiringPi
     if(wiringPiSetup() == -1) //when initialize wiringPi failed, print message to screen
     { 
@@ -45,22 +94,23 @@ int main()
     return -1; 
   }
 
-/*
+
   // Initialisation des périphériques i2c au moyen de wiringPiI2CSetup
-  int devIdTDABass = 0x43;
-  int devIdTDATrebble = 0x42;
-  // création de l'objet I2C à l'adresse 0x43 (pour TDA bass)
+  int devIdTDABass = 0x86;
+  int devIdTDATrebble = 0x84;
+  // création de l'objet I2C à l'adresse 0x86 (pour TDA bass)
 	int I2C_TDABASS = wiringPiI2CSetup(devIdTDABass);
   // test validant la création de l'objet I2C
-    if(I2C_TDABASS < 0) cout << "Error. TDA bass Errno is: " << errno << endl;
-    else  cout << "Init result for TDA bass: "<< I2C_TDABASS << endl;
-    // création de l'objet I2C à l'adresse 0x42 (pour TDA trebble)
+    if(I2C_TDABASS < 0) cout << "Erreur avec la connexion au TDA bass | Errno is: " << errno << endl;
+    else  cout << "Connexion reussie avec TDA bass | "<< I2C_TDABASS << endl;
+  /*  
+    // création de l'objet I2C à l'adresse 0x82 (pour TDA trebble)
   int I2C_TDATREBL = wiringPiI2CSetup(devIdTDATrebble);
   // test validant la création de l'objet I2C
     if(I2C_TDATREBL < 0) cout << "Error. TDA trebble Errno is: " << errno << endl;
     else  cout << "Init result for TDA trebble: "<< I2C_TDATREBL << endl;
 
-
+/*
   // Initialisation des périphériques SPI au moyen de wiringPiSPISetup
   int devIdAD1;
   int devIdAD2;
@@ -79,12 +129,14 @@ int main()
     
   // Initialisation de l'ensemble des pin utilisées comme GPIO
   pinMode(RelayPin, OUTPUT);
+  //pullUpDnControl(pin, mode) permet de configurer une pin en mode pull-up, pull-down ...
 
   // Boucle infinie du programme
   do
   {
     cin >> state;
     if(state == 0 || state == 1) digitalWrite(RelayPin, state);
+
   }
   while(1);
   
