d622d97b54e39fe6824ca8d7097e2d40e0deb43a
SpellAudio <philippe.guelen@gmail.com>
2018-04-27 16:59:56 +0200

    SPI fonctionnel LED BLINK

--- src/main.cpp
+++ src/main.cpp
@@ -34,32 +34,21 @@ using namespace std;
 // Definition des pin GPIO utilisées
 #define RelayPin 29
 #define ADChipSelect 10
+#define SPION 0x00
+#define SPIOFF 0xFF
 
-
-/* I2C Start condition
-* SCL HIGH 
-* SDA HIGH -> LOW
-*
-* I2C Stop condition
-* SCL LOW -> HIGH
-* SDA LOW -> HIGH
-*
-* Ne jamais modifier l'état de SCL pendant un changement d'état de SDA
-* Modifier SDA que quand SCL est LOW
-* 
-* Mettre SDA en HIGH au moment du ACKNOWLEDGE. LE TDA passera SDA à LOW si ACK
-*
-* PULL UP sur SDA et SCL
-*/
+char devIdTDABass = 0x43;
+char devIdTDATrebble = 0x42;
+int channelAD1 = 0;
+int channelAD2 = 1;
+int speed = 1000000;
 
 void setPot(int pot, int level)
 {
   uint8_t buff[2];
   buff[0] = pot;
   buff[1] = level;
-  digitalWrite(ADChipSelect, 0); //active le composant
   wiringPiSPIDataRW(0, buff, 2);
-  digitalWrite(ADChipSelect, 1); //désactive le composant
 }
 
 void allOff()
@@ -83,91 +72,44 @@ int main()
 {
   // Définition des variables du programme
   int state = 0; // Niveau du relais: 1 = fermé (contact) 0 = ouvert (pas contact)
-  unsigned char init;     // Lecture des conditions d'activation des périphériques série
-  //char read[8]; // Lecture des commande hexa opur TDA
   uint8_t read;
-  int ack; // int recu apres un wiringPiI2CWrite
+  unsigned char buff[2];
+  buff[0] = 0x00;
 
   // Initialisation de wiringPi
-  if(wiringPiSetup() == -1) //when initialize wiringPi failed, print message to screen
-  { 
-    printf("setup wiringPi failed !\n");
-    return -1; 
-  }
+  wiringPiSetup();
 
   // Initialisation de l'ensemble des pin utilisées comme GPIO
   pinMode(RelayPin, OUTPUT);
-  pullUpDnControl(8, PUD_UP); // SDA
-  pullUpDnControl(9, PUD_UP); // SCL
-  // Initialisation des périphériques i2c au moyen de wiringPiI2CSetup
-  char devIdTDABass = 0x43;
-  char devIdTDATrebble = 0x42;
+  pinMode(ADChipSelect, OUTPUT);
 
-  // Initialisation des périphériques série
-  // création de l'objet I2C à l'adresse 0x86 (pour TDA bass)
-	int fd = wiringPiI2CSetup((int)devIdTDABass);
-  ack = wiringPiI2CWrite(fd,1000110);
-  cout << "I2C_TDABASS CONFIGURE = " << fd << "ack = " << ack << endl;
-  // test validant la création de l'objet I2C
-  if(fd < 0) 
-    cout << "Erreur avec la connexion au TDA bass | Errno is: " << errno << endl;
+  // Initialisation des périphériques I2C
+	wiringPiI2CSetup((int)devIdTDABass);
+  wiringPiI2CSetup((int)devIdTDATrebble);
 
-    // création de l'objet I2C à l'adresse 0x82 (pour TDA trebble)
-  int I2C_TDATREBL = wiringPiI2CSetup((int)devIdTDATrebble);
-  cout << "I2C_TDATREBL CONFIGURE = " << I2C_TDATREBL << endl;
-  // test validant la création de l'objet I2C
-  if(I2C_TDATREBL < 0) 
-    cout << "Error. TDA trebble Errno is: " << errno << endl;
+  //Initialisation des périphériques SPI
+  wiringPiSPISetup(channelAD1, speed);
+  wiringPiSPISetup(channelAD2, speed);
 
-  //Initialisation des périphériques SPI au moyen de wiringPiSPISetup
-  int devIdAD1 = 0;
-  int devIdAD2 = 1;
-  int speed = 500000;
-
-  cout << "Utiliser le AD avec l'addresse  ? y/n" << endl;
-  cin >> init;
-  if(init == 'y')
-  {
-    // création de l'objet SPI à l'adresse (premier digipot)
-    int SPI_AD1 = wiringPiSPISetup(devIdAD1, speed);
-    // test validant la création de l'objet SPI
-      if(SPI_AD1 < 0) cout << "Error. AD1 creation  Errno is: " << errno << endl;
-    //création de l'objet SPI à l'adresse (deuxième digipot)
-    int SPI_AD2 = wiringPiSPISetup(devIdAD2, speed);
-    // test validant la création de l'objet SPI
-      if(SPI_AD2 < 0) cout << "Error. AD2 Errno is: " << errno << endl;
-  }
-  
   cout << "Fin de l'initialisation des périphériques série" << endl;
-  uint8_t buff[2];
-  buff[0] = 0;
+
   // Boucle infinie du programme
   while(true)
   {
     /*
     cin >> read; 
-    /*
-    ack = wiringPiI2CWrite(fd,read);
-    cout << " ack = " << ack << endl;
-    if( ack < 0) cout << "Communication ratée" << endl;
-    else cout << "Byte envoyé: " << read << endl;*/
-
+    wiringPiI2CWrite(fd,read);*/
 
     cout<<"Blink all"<<endl;
-    digitalWrite(ADChipSelect, 0); //active le composant   
-    //allOn();
-    buff[1] = 255;
-    wiringPiSPIDataRW(0, buff, 2);
-    digitalWrite(ADChipSelect, 1); //désactive le composant
-    usleep(1000*1000);
+    buff[1] = SPION;
+    wiringPiSPIDataRW(channelAD1, buff, sizeof(buff));
+    usleep(1000*5000);
 
 
     cout<<"Shut all"<<endl;
-    digitalWrite(ADChipSelect, 0); //active le composant   
     //allOff();
-    buff[1] = 0;
-    wiringPiSPIDataRW(0, 0, 2);
-    digitalWrite(ADChipSelect, 1); //désactive le composant
+    buff[1] = SPIOFF;
+    wiringPiSPIDataRW(channelAD1, buff, sizeof(buff));
     usleep(1000*1000);
 
   }
